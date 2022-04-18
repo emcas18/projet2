11c467ed3b64ab2c698a58b06fb71a0cca985ddc
emcas18 <73842558+emcas18@users.noreply.github.com>
2022-04-18 19:43:08 -0400

    Add files via upload

---
+++ moncodeprojet2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.decomposition import PCA
+from scipy import interpolate
+from BaselineRemoval import BaselineRemoval
+
+
+my_file = open(r'C:\Users\emili\OneDrive\Bureau\Projet 2 optique\alcool 701.txt', "r")
+content = my_file.read()
+content_list = content.split("\n")
+my_file.close()
+
+data = []
+for i in content_list:
+    x = i.split(',')
+    data.append(x)
+
+for j in data:
+    if len(j) < 3:
+        data.remove(j)
+
+
+intensite = []
+longueur = []
+for k in range(0,1100):
+    intensite.append(int(data[k][1]))
+    longueur.append(int(float(data[k][2])))
+
+plt.plot(intensite, longueur)
+plt.show()
